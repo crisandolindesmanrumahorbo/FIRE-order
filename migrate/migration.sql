0686005c37fe4b6e0a61be55137166b173ae73a4
crisandolindesmanrumahorbo <crisandolin007@gmail.com>
2025-05-03 14:55:14 +0700

    migration script

---
+++ migrate/migration.sql
@@ -0,0 +1,15 @@
+CREATE TABLE orders (
+  order_id SERIAL PRIMARY KEY,
+  product_symbol VARCHAR(10) NOT NULL,
+  product_name VARCHAR(100) NOT NULL,
+  side VARCHAR(1) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  lot INT NOT NULL,
+  expiry VARCHAR(50) NOT NULL,
+  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+  user_id INT NOT NULL,
+  product_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+  FOREIGN KEY (product_id) REFERENCES products(product_id)
+  CONSTRAINT unique_user_product UNIQUE (user_id, product_id)
+);
