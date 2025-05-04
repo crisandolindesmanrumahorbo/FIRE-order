8df022046844a00d34efa66ca0d8440a4d7d0e2d
crisandolindesmanrumahorbo <crisandolin007@gmail.com>
2025-05-04 13:18:37 +0700

    change jwt behavior

--- shared/auth_validate/src/jwt.rs
+++ shared/auth_validate/src/jwt.rs
@@ -1,7 +1,5 @@
-use dotenvy::dotenv;
 use jsonwebtoken::{Algorithm, DecodingKey, Validation, decode};
 use serde::{Deserialize, Serialize};
-use std::env;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Claims {
@@ -9,23 +7,17 @@ struct Claims {
     exp: usize,
 }
 
-pub fn verify_jwt(token: &str) -> Result<String, &'static str> {
-    let public_key = get_public_key();
-
+pub fn verify_jwt(token: &str, public_key: &str) -> Result<String, &'static str> {
+    let dec_key = DecodingKey::from_rsa_pem(public_key.replace("\\n", "\n").as_bytes())
+        .expect("Invalid public key");
     let mut validation = Validation::new(Algorithm::RS256);
     validation.validate_exp = true; // Ensure expiration is checked
     validation.validate_aud = false; // Disable audience check (optional)
 
-    let token_data = decode::<Claims>(token, &public_key, &validation).map_err(|e| {
+    let token_data = decode::<Claims>(token, &dec_key, &validation).map_err(|e| {
         println!("JWT error: {:?}", e); // Debugging
         "Invalid token"
     })?;
 
     Ok(token_data.claims.sub)
-}
-
-fn get_public_key() -> DecodingKey {
-    dotenv().ok();
-    let key = env::var("JWT_PUBLIC_KEY").expect("Missing JWT_PUBLIC_KEY");
-    DecodingKey::from_rsa_pem(key.replace("\\n", "\n").as_bytes()).expect("Invalid public key")
 }
